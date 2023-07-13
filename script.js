1caa5cffd166ffd4505e249905ee875a4432b026
DuskDuck <nhuduc12754@gmail.com>
2023-07-13 11:17:12 +0700

    add base form

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const scroller = document.querySelector('.scroller');
+    const sections = document.querySelectorAll('.content');
+    
+    function fadeIn(section) {
+        section.classList.add('fade-in');
+    }
+    
+    function isElementInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        console.log(rect.bottom);
+        return (
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        );
+    }
+    
+    function handleScroll() {
+        sections.forEach(function(section) {
+            if (isElementInViewport(section)) { fadeIn(section);}
+        });
+    }
+    
+    scroller.addEventListener('scroll', handleScroll);
+    handleScroll();
+});
